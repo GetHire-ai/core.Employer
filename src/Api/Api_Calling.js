ac09ad31702f1a497023931ef64e46a018bf563c
Mohammed Owais <filledstackdeveloper@gmail.com>
2024-10-06 13:19:47 +0530

    changes

--- src/Api/Api_Calling.js
+++ src/Api/Api_Calling.js
@@ -1,7 +1,5 @@
 import axios from "axios";
-// const Api_Url = "https://get-hire-backend.vercel.app/";
-// const Api_Url = process.env.REACT_APP_BACKEND_URL
-const Api_Url = "http://localhost:5000/";
+const Api_Url = process.env.REACT_APP_BACKEND_URL
 
 const checkToken = (message) => {
   if (message === "Unauthorized") {
