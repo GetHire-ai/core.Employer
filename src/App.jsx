e8ccb119bbda9309860ee56d4cf93c70fdd1f421
Mohammed Owais <filledstackdeveloper@gmail.com>
2024-10-15 11:37:29 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ import Community from "./pages/Community/index";
 import JobCreateManual from "pages/Jobs/JobsCreate/JobCreateManual";
 import JobEdit from "pages/Jobs/JobsCreate/EditAJobWithManual";
 import JobCreateManualId from "pages/Jobs/JobsCreate/JobCreateManualId";
-import Chat from "./pages";
+import Chat from "./pages/Chat";
 import FirstPage from "pages/Auth/FirstPage";
 import Forget from "pages/Auth/Forget";
 import SearchCandidate from "pages/Jobs/SearchCandidate/SearchCandidate";
