15f89addb1dcaaec4fc02db2127c3c5930c124c0
Mohammed Owais <filledstackdeveloper@gmail.com>
2024-10-21 16:50:41 +0530

    changes

--- src/pages/JobsDashboard/index.jsx
+++ src/pages/JobsDashboard/index.jsx
@@ -352,82 +352,6 @@ const DashboardPage = () => {
                     />
                   </div>
                 </div>
-
-                {/* <div className="flex justify-between w-full text-gray-600 mb-5 h-14  divide-x divide-gray-300">
-                  <div
-                    className="cursor-pointer -mr-7 px-6"
-                    // onClick={() => navigate(`/jobsApplication/${job._id}`)}
-                    onClick={() =>
-                      navigate(`/jobsApplicationManager2/${job._id}`)
-                    }
-                  >
-                    <span className="border-orange-500 py-3">
-                      {job?.totalApplicationCount}
-                    </span>
-                    <h2 className="mt-2">Applications</h2>
-                  </div>
-                  <div className="-mr-5 px-6">
-                    <span className="border-blue-500 py-3">
-                      {job?.shortlistedApplicationCount}
-                    </span>
-                    <h2 className="mt-2">In Process</h2>
-                  </div>
-                  <div className="px-6 mr-2">
-                    <span className="border-green-500 py-3">
-                      {job?.selectedStudentCount}
-                    </span>
-                    <h2 className="mt-2">Hired</h2>
-                  </div>
-                </div> */}
-                {/* <div className="flex justify-between w-full text-gray-600 mb-5 h-14">
-                      <div
-                        className="flex flex-col items-center cursor-pointer px-6"
-                        onClick={() => navigate(`/jobsApplicationManager2/${job._id}`)}
-                      >
-                        <span className="text-2xl font-semibold text-orange-500">
-                          {job?.totalApplicationCount}
-                        </span>
-                        <h2 className="mt-1 border-t-2 border-orange-500 w-full text-center">
-                          Applications
-                        </h2>
-                      </div>
-                      <div className="flex flex-col items-center px-6">
-                        <span className="text-2xl font-semibold text-blue-500">
-                          {job?.shortlistedApplicationCount}
-                        </span>
-                        <h2 className="mt-1 border-t-2 border-blue-500 w-full text-center">
-                          In Process
-                        </h2>
-                      </div>
-                      <div className="flex flex-col items-center px-6">
-                        <span className="text-2xl font-semibold text-green-500">
-                          {job?.selectedStudentCount}
-                        </span>
-                        <h2 className="mt-1 border-t-2 border-green-500 w-full text-center">
-                          Hired
-                        </h2>
-                      </div>
-                    </div> */}
-                {/* <div className="flex justify-between w-full text-gray-600 mb-5 h-14">
-                    <div
-                      className="flex flex-col items-center cursor-pointer px-6"
-                      onClick={() => navigate(`/jobsApplicationManager2/${job._id}`)}
-                      >
-                      <span className="text-xl font-semibold">{job?.totalApplicationCount}</span>
-                      <div className="w-full border-t-2 border-orange-500 mt-1" />
-                      <h2 className="text-sm mt-1">Applications</h2>
-                    </div>
-                    <div className="flex flex-col items-center px-6">
-                      <span className="text-xl font-semibold">{job?.shortlistedApplicationCount}</span>
-                      <div className="w-full border-t-2 border-blue-500 mt-1" />
-                      <h2 className="text-sm mt-1">In Process</h2>
-                    </div>
-                    <div className="flex flex-col items-center px-6">
-                      <span className="text-xl font-semibold">{job?.selectedStudentCount}</span>
-                      <div className="w-full border-t-2 border-green-500 mt-1" />
-                      <h2 className="text-sm mt-1">Hired</h2>
-                    </div>
-                  </div> */}
                 <div className="flex justify-between w-full text-gray-600 mb-5">
                   <div
                     className="flex flex-col items-center cursor-pointer px-6"
@@ -446,7 +370,7 @@ const DashboardPage = () => {
                       {job?.shortlistedApplicationCount}
                     </span>
                     <div className="w-32 border-t-2 border-blue-500 mt-2" />
-                    <h2 className="text-sm mt-4">In Process</h2>
+                    <h2 className="text-sm mt-4">Shortlisted</h2>
                   </div>
                   <div className="flex flex-col items-center px-6">
                     <span className="text-xl font-medium">
